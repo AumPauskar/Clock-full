e2e2c60eac15086c79fdc405288afac85ddfa890
Aum_Pauskar <aumpauskar111@gmail.com>
2021-09-09 10:57:42 +0530

    Added appropriate label for stopwatch and timer

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ canvas_timer = Canvas(root)
 canvas_timer.pack()
 
 
-# --------------------------------------------
+# --------------------
 # this section of code will handle the labels of the main clock part
 # the time will contain time in the form of 07:47:09
 # aka in hours:minutes:secoonds
@@ -34,7 +34,48 @@ label_time_min_dot.grid(row = 1, column = 4)
 # this paragraph of code will contain the seconds part
 label_time_sec = Label(canvas_time, text = '')
 label_time_sec.grid(row = 1, column = 5)
-# ----------------------------------------------
+# --------------------
+
+
+# --------------------
+# this part will contain the script for the stopwatch part
+# the time will be displayed in a hour:minute:second fashion
+# this paragraph will contain the hours part
+label_stopwatch_hr = Label(canvas_stopwatch, text = '')
+label_stopwatch_hr.grid(row = 1, column = 1)
+label_stopwatch_hr_dot = Label(canvas_stopwatch, text = ':')
+label_stopwatch_hr_dot.grid(row = 1, column = 2)
+
+# this part will contain the minutes part
+label_stopwatch_min = Label(canvas_stopwatch, text = '')
+label_stopwatch_min.grid(row = 1, column = 3)
+label_stopwatch_min_dot = Label(canvas_stopwatch, text = ':')
+label_stopwatch_min_dot.grid(row = 1, column = 4)
+
+# this part will contain the seconds part
+label_stopwatch_sec = Label(canvas_stopwatch, text ='')
+label_stopwatch_sec.grid(row = 1, column = 5)
+# --------------------
+
+# --------------------
+# this part will contain the countdown-timer part
+# the time will be displayed in a hour:minute:second fashion
+# this paragraph of code will contain the hours part
+label_timer_hr = Label(canvas_timer, text ='')
+label_timer_hr.grid(row = 1, column = 1)
+label_timer_hr_dot = Label(canvas_timer, text = ':')
+label_timer_hr_dot.grid(row = 1, column = 2)
+
+# this paragraph of code will contain the minutes part
+label_timer_min = Label(canvas_timer, text ='')
+label_timer_min.grid(row = 1, column = 3)
+label_time_min_dot = Label(canvas_timer, text = ':')
+label_time_min_dot.grid(row = 1, column = 4)
+
+# this paragraph of code will contain the seconds part
+label_time_sec = Label(canvas, text = '')
+label_time_sec.grid(row = 1, column = 5)
+# --------------------
 
 # tkinter essential
 root.mainloop()
