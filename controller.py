b568b0bd3ebf9090c93e56fa6c9a07e2e6c2c211
Aum_Pauskar <aumpauskar111@gmail.com>
2021-09-09 22:51:33 +0530

    Basic timer added

--- controller.py
+++ controller.py
@@ -3,3 +3,24 @@ def GiveLayout():
 	with open('settings.json', 'r') as file:
 		data = json.load(file)
 		return data['layout']
+
+def Convert(hours, minutes, seconds):
+	# these two if statements are executed to convert the times in the correct form
+	# eg if seconds == 3600 then hour ==1 and minutes, seconds are reduced to 0
+	if seconds >= 60:
+		temp = seconds // 60
+		seconds = seconds % 60
+		minutes += temp
+	if minutes >= 60:
+		temp = minutes // 60
+		minutes = minutes % 60
+		hours += temp
+
+	# these two if statements does not allow for minutes and seconds to be of length 1
+	seconds = str(seconds); minutes = str(minutes); hours = str(hours)
+	if len(seconds) == 1:
+		seconds = '0' + seconds
+	if len(minutes) == 1:
+		minutes = '0' + minutes
+
+	return hours, minutes, seconds
--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 from tkinter import *
 import time
 import controller as co
+import json
+
+with open('settings.json', 'r') as val:
+	data = json.load(val)
+	hours_timer = int(data['timer_hrs']); minutes_timer = int(data['timer_min']); seconds_timer = int(data['timer_sec'])
+timer = co.Convert(hours_timer, minutes_timer, seconds_timer)
 
 geometry_size = co.GiveLayout()
 
@@ -20,13 +26,45 @@ canvas_timer = Canvas(root)
 canvas_timer.pack()
 
 def RunClock():
-    hour = time.strftime('%H')
-    minute = time.strftime('%M')
-    second = time.strftime('%S')
-    label_time_hr.config(text = hour)
-    label_time_min.config(text = minute)
-    label_time_sec.config(text = second)
+    hour_clock = time.strftime('%H')
+    minute_clock = time.strftime('%M')
+    second_clock = time.strftime('%S')
+    label_time_hr.config(text = hour_clock)
+    label_time_min.config(text = minute_clock)
+    label_time_sec.config(text = second_clock)
     root.after(1000, RunClock)
+
+def RunTimer():
+    global hours_timer, minutes_timer, seconds_timer
+    if hours_timer == minutes_timer == seconds_timer == 0:
+        minutes_timer = 'Time'; seconds_timer = 'up'
+        label_timer_hr.grid_forget()
+        label_timer_hr_dot.grid_forget()
+        label_timer_min_dot.grid_forget()
+    elif minutes_timer == seconds_timer == 0:
+        hours_timer -=1 
+        minutes_timer = seconds_timer = 59
+        root.after(1000, RunTimer)
+    elif seconds_timer == 0:
+        minutes_timer -= 1
+        seconds_timer == 59
+        root.after(1000, RunTimer)
+    else:
+        seconds_timer -= 1
+        root.after(1000, RunTimer)
+
+    if len(str(seconds_timer)) == 1:
+        temp_timer_sec = '0' + str(seconds_timer)
+    else:
+        temp_timer_sec = str(seconds_timer)
+    if len(str(minutes_timer)) == 1:
+        temp_timer_min = '0' + str(minutes_timer)
+    else:
+        temp_timer_min = str(minutes_timer)
+    label_timer_hr.config(text = hours_timer)
+    label_timer_min.config(text = minutes_timer)
+    label_timer_sec.config(text =seconds_timer)
+    
 # --------------------
 # this section of code will handle the labels of the main clock part
 # the time will contain time in the form of 07:47:09
@@ -89,7 +127,7 @@ label_timer_sec = Label(canvas_timer, text = '')
 label_timer_sec.grid(row = 1, column = 5)
 # --------------------
 
-
+RunTimer()
 RunClock()
 # tkinter essential
 root.mainloop()
